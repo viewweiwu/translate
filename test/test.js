949724d32c7d10ba1e2272ff765b8e34573484a3
648764190@qq.com <648764190@qq.com>
2018-07-18 15:24:25 +0800

    支持数组类型

--- test/test.js
+++ test/test.js
@@ -6,15 +6,27 @@ let expect = chai.expect
 
 describe('test translate', () => {
   it('translate({ fooBar: 1 }) 应该要返回 { foo_bar: 1 }', () => {
-    expect(translate({ fooBar: 1 })).to.be.deep.equal({ foo_bar: 1 })
+    expect(translate({ fooBar: 1 }))
+      .to.be.deep.equal({ foo_bar: 1 })
   })
   it('translate({ FooBar: 1 }) 应该要返回 { foo_bar: 1 }', () => {
-    expect(translate({ FooBar: 1 })).to.be.deep.equal({ foo_bar: 1 })
+    expect(translate({ FooBar: 1 }))
+      .to.be.deep.equal({ foo_bar: 1 })
   })
   it('translate({ fooBarAbc: 1 }) 应该要返回 { foo_bar_abc: 1 }', () => {
-    expect(translate({ fooBarAbc: 1 })).to.be.deep.equal({ foo_bar_abc: 1 })
+    expect(translate({ fooBarAbc: 1 }))
+      .to.be.deep.equal({ foo_bar_abc: 1 })
   })
   it('translate({ fooBar: 1, childrenObj: { helloAbc: 1 } }) 应该要返回 { foo_bar: 1, children_obj: { hello_abc: 1 } }', () => {
-    expect(translate({ fooBar: 1, childrenObj: { helloAbc: 1 } })).to.be.deep.equal({ foo_bar: 1, children_obj: { hello_abc: 1 } })
+    expect(translate({ fooBar: 1, childrenObj: { helloAbc: 1 } }))
+      .to.be.deep.equal({ foo_bar: 1, children_obj: { hello_abc: 1 } })
+  })
+  it('translate({ fooBar: [{abcEfg: 1}] }) 应该要返回 { foo_bar: [{abc_efg: 1}] }', () => {
+    expect(translate({ fooBar: [{abcEfg: 1}] }))
+      .to.have.deep.equal({ foo_bar: [{abc_efg: 1}] })
+  })
+  it('translate({ fooBar: [[{abcEfg: 1}]] }) 应该要返回 { fooBar: [[{abc_efg: 1}]] }', () => {
+    expect(translate({ fooBar: [[{abcEfg: 1}]] }))
+      .to.be.deep.equal({ foo_bar: [[{abc_efg: 1}]] })
   })
 })
--- translate.js
+++ translate.js
@@ -1,11 +1,19 @@
 const translate = (obj = {}) => {
+  if (Array.isArray(obj)) {
+    return obj.map(item => translate(item))
+  } else if (!isObject(obj)) {
+    return obj
+  }
   let result = {}
   for (let key in obj) {
     let newKey = getNewKey(key)
+    let value = obj[key]
 
-    result[newKey] = isPlainObject(obj[key])
-      ? translate(obj[key])
-      : obj[key]
+    if (Array.isArray(value) || isObject(value)) {
+      result[newKey] = translate(value)
+    } else {
+      result[newKey] = value
+    }
   }
   return result
 }
@@ -14,6 +22,10 @@ const getNewKey = (key) => {
   let newKey = key.replace(/([A-Z])/g, '_$1').toLowerCase()
   newKey.startsWith('_') && (newKey = newKey.slice(1))
   return newKey
+}
+
+const isObject = (obj) => {
+  return Object.prototype.toString.call(obj) === '[object Object]'
 }
 
 export default translate
